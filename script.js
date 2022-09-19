4ef4c5c7e4e082116563888f6b525b5125662d04
Sabrina5962 <sabrinastievet@gmail.com>
2022-09-19 13:54:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let ville = "Cambrai";
+const hypotenuse = 8;
+let admission = true;
+let monNom = "Dupont";
+let monPrenom = "Paul";
+let monAge = 35;
+alert(monNom)
+
+/*  pour faire un commentaire */
+
+var pommes = 4;
+var pêches = 6;
+var totalfruits = pommes + pêches;
+console.log(totalfruits);
+
+function addition(a, b) {
+    return (a + b);
+   }
+alert(addition(5,8));
+
+
+function soustraction(a, b) {
+    return (a - b);
+}
+alert(soustraction(10,5));
+
+function multiplication(a, b) {
+    return (a * b);
+ ;
+}
+alert(multiplication(10,5));
+
+function division(a, b) {
+    return (a/b);
+}
+alert(multiplication(100,2));
+
+let firstName = "Stievet";
+let lastName = "Sabrina";
+
+const person = {
+    firstName: "Stievet",
+    lastName: "Sabrina"
+
+}
+alert (firstName.lastName)
+
+
